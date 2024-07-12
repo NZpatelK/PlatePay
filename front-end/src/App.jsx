1aa7bf5c9b797a290f77433e7d5e06704054bb74
NZpatelK <patelk.dev@hotmail.com>
2024-07-12 18:30:30 +1200

    UPDATE: add the drag and drop function.

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,44 +1,46 @@
-import { useEffect, useState } from 'react';
-import axios from 'axios';
-import socketIOClient from 'socket.io-client';
+// import { useEffect, useState } from 'react';
+// import axios from 'axios';
+// import socketIOClient from 'socket.io-client';
 import './App.css';
+import DragAndDrop from './components/dragAndDrop';
 
-const ENDPOINT = "http://127.0.0.1:5000"; // Flask server endpoint
+// const ENDPOINT = "http://127.0.0.1:5000"; // Flask server endpoint
 
 function App() {
-  const [httpResponse, setHttpResponse] = useState("");
-  const [socketResponse, setSocketResponse] = useState("");
-  const [message, setMessage] = useState("");
+  // const [httpResponse, setHttpResponse] = useState("");
+  // const [socketResponse, setSocketResponse] = useState("");
+  // const [message, setMessage] = useState("");
 
-  useEffect(() => {
-    // HTTP request to Flask server
-    axios.get(`${ENDPOINT}/api/data`)
-      .then(response => {
-        setHttpResponse(response.data);
-        console.log(response.data);
-      })
-      .catch(error => {
-        console.error("There was an error making the request!", error);
-      });
+  // useEffect(() => {
+  //   // HTTP request to Flask server
+  //   axios.get(`${ENDPOINT}/api/data`)
+  //     .then(response => {
+  //       setHttpResponse(response.data);
+  //       console.log(response.data);
+  //     })
+  //     .catch(error => {
+  //       console.error("There was an error making the request!", error);
+  //     });
 
-    // Socket.IO connection
-    const socket = socketIOClient(ENDPOINT);
-    socket.on("message", data => {
-      setSocketResponse(data);
-    });
+  //   // Socket.IO connection
+  //   const socket = socketIOClient(ENDPOINT);
+  //   socket.on("message", data => {
+  //     setSocketResponse(data);
+  //   });
 
-    return () => socket.disconnect();
-  }, []);
+  //   return () => socket.disconnect();
+  // }, []);
 
-  const sendMessage = () => {
-    const socket = socketIOClient(ENDPOINT);
-    socket.send(message);
-    setMessage("");
-  };
+  // const sendMessage = () => {
+  //   const socket = socketIOClient(ENDPOINT);
+  //   socket.send(message);
+  //   setMessage("");
+  // };
 
   return (
     <div>
-      <h1>React and Flask Communication</h1>
+      <DragAndDrop/>
+      {/* <h1>React and Flask Communication</h1>
       <div>
         <h2>HTTP Response</h2>
         <p>{httpResponse}</p>
@@ -52,7 +54,7 @@ function App() {
         value={message} 
         onChange={(e) => setMessage(e.target.value)} 
       />
-      <button onClick={sendMessage}>Send Message</button>
+      <button onClick={sendMessage}>Send Message</button> */}
     </div>
   );
 }
--- front-end/src/components/dragAndDrop.jsx
+++ front-end/src/components/dragAndDrop.jsx
@@ -1,16 +1,78 @@
-import React from 'react';
+import { useState } from 'react';
+import './dragAndDrop.css';
 
-const NewPage = () => {
+const DragAndDrop = () => {
+    const [isDragOver, setIsDragOver] = useState(false);
+    const [uploadedImage, setUploadedImage] = useState('');
+    const [isImageUploaded, setIsImageUploaded] = useState(false);
+
+    const handleDragOver = (e) => {
+        e.preventDefault();
+        setIsDragOver(true);
+    };
+
+    const handleDragLeave = (e) => {
+        e.preventDefault();
+        setIsDragOver(false);
+    };
+
+    const handleDrop = (e) => {
+        e.preventDefault();
+        const image = e.dataTransfer.files[0];
+        handleFileUpload(image);
+    };
+
+    const handleFileUpload = (file) => {
+        const validExtensions = ["image/jpeg", "image/jpg", "image/png"];
+        if (validExtensions.includes(file.type)) {
+            const reader = new FileReader();
+            reader.onloadend = () => {
+                setUploadedImage(reader.result);
+                setIsImageUploaded(true);
+            };
+            reader.readAsDataURL(file);
+        } else {
+            setIsImageUploaded(false);
+        }
+    };
+
+    const handleDelete = () => {
+        setUploadedImage('');
+        setIsImageUploaded(false);
+    };
+
     return (
-        <div className="dragger_wrapper">
-            <div className="dragger">
-                
-            </div>
-            <div className="fileName">
+        <div className="dragger-wrapper">
+            <div
+                className={`dragger ${isImageUploaded ? 'active' : ''}`}
+                id="dragger"
+                onDragOver={handleDragOver}
+                onDragLeave={handleDragLeave}
+                onDrop={handleDrop}
+            >
+                {isImageUploaded ? (
+                    <img src={uploadedImage} alt="" />
+                ) : (
+                    <>
+                        <h2>{isDragOver ? 'Release to Upload Image' : 'Drag and Drop Files'}</h2>
+                        <h3>or</h3>
 
+                        <button className="browse-file" id="browse-file">
+                            Browse File
+                        </button>
+                        <input type="file" hidden id="file-input-field" />
+                    </>
+                )}
+            </div>
+            <div className="file-name" id="file-name">
+                {isImageUploaded ? (
+                    <i className="fas fa-trash-can" onClick={handleDelete}>delete</i>
+                ) : (
+                    ''
+                )}
             </div>
         </div>
     );
 };
 
-export default NewPage;
+export default DragAndDrop;
