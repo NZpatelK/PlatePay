05d4d1756a4ea6501a42ff97a9e6f41cda3c94c9
NZpatelK <patelk.dev@hotmail.com>
2024-07-12 16:49:06 +1200

    create a new feature of drag and drop

---
+++ front-end/src/components/dragAndDrop.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const NewPage = () => {
+    return (
+        <div className="dragger_wrapper">
+            <div className="dragger">
+                
+            </div>
+            <div className="fileName">
+
+            </div>
+        </div>
+    );
+};
+
+export default NewPage;
