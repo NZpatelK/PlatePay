ec4c91c9a09a23b9b9192ae88e6ffac4a1c7c6b0
NZpatelK <patelk.dev@hotmail.com>
2024-07-12 19:52:16 +1200

    add comment

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -7,37 +7,33 @@ import DragAndDrop from './components/dragAndDrop';
 const socket = io('http://127.0.0.1:5000');
 
 function App() {
-  const [selectedFile, setSelectedFile] = useState(null);
+  const [selectedImage, setSelectedImage] = useState(null);
   const [plateNumber, setPlateNumber] = useState('');
 
   useEffect(() => {
-    socket.on('plate_recognized', (data) => {
-      setPlateNumber(data.plate_number);
-      console.log(data.plate_number);
-    });
+    const handlePlateRecognized = (data) => {
+      setPlateNumber(data.plateNumber);
+    };
+
+    socket.on('plate_recognized', handlePlateRecognized);
 
     return () => {
-      socket.off('plate_recognized');
+      socket.off('plate_recognized', handlePlateRecognized);
     };
   }, []);
 
-  const handleFileChange = (file) => {
-    setSelectedFile(file);
-    console.log("file: ", file);
+  const handleImageChange = (image) => {
+    setSelectedImage(image);
   };
 
   const handleSubmit = async (event) => {
     event.preventDefault();
-    console.log("selectedFile: ", selectedFile);
+
     const formData = new FormData();
-    formData.append('image', selectedFile);
+    formData.append('image', selectedImage);
 
     try {
-      await axios.post('http://127.0.0.1:5000/api/upload', formData, {
-        headers: {
-          'Content-Type': 'multipart/form-data'
-        }
-      });
+      await axios.post('http://127.0.0.1:5000/api/upload', formData);
     } catch (error) {
       console.error('Error uploading image:', error);
     }
@@ -46,17 +42,11 @@ function App() {
   return (
     <div>
       <h1 className='title'>Number Plate Recognition</h1>
-      <DragAndDrop imageFile={handleFileChange} />
-      <button className='submit' onClick={handleSubmit}>Upload and Recognise</button>
+      <DragAndDrop imageFile={handleImageChange} />
+      <button className='submit' onClick={handleSubmit}>
+        Upload and Recognise
+      </button>
       {plateNumber && <div>Recognised Plate Number: {plateNumber}</div>}
-
-
-
-      {/* <form onSubmit={handleSubmit}>
-        <input type="file" onChange={handleFileChange} />
-        <button type="submit">Upload and Recognize</button>
-      </form>
-      {plateNumber && <div>Recognized Plate Number: {plateNumber}</div>} */}
     </div>
   );
 }
--- front-end/src/components/dragAndDrop.jsx
+++ front-end/src/components/dragAndDrop.jsx
@@ -2,79 +2,102 @@ import { useState } from 'react';
 import './dragAndDrop.css';
 import PropTypes from 'prop-types';
 
+/**
+ * DragAndDrop component is responsible for handling image upload and drag and drop functionality.
+ * It uses React hooks to manage state and renders a drag and drop area and a file input field.
+ *
+ * @param {function} imageFile - A function that takes an image file and is called when an image is uploaded.
+ * @returns {JSX.Element} - The rendered DragAndDrop component.
+ */
 const DragAndDrop = ({ imageFile }) => {
-    const [isDragOver, setIsDragOver] = useState(false);
-    const [uploadedImage, setUploadedImage] = useState('');
-    const [isImageUploaded, setIsImageUploaded] = useState(false);
+  // State variables to manage the drag and drop area and image upload
+  const [isDraggedOver, setIsDraggedOver] = useState(false); // Whether the drag and drop area is being hovered over
+  const [imageDataUrl, setImageDataUrl] = useState(''); // The data URL of the uploaded image
+  const [isImageUploaded, setIsImageUploaded] = useState(false); // Whether an image has been uploaded
 
-    const handleDragOver = (e) => {
-        e.preventDefault();
-        setIsDragOver(true);
-    };
+  /**
+   * Event handler for the 'dragover' event. Prevents the default behavior and sets isDraggedOver to true.
+   *
+   * @param {Event} event - The dragover event.
+   */
+  const handleDragOver = (event) => {
+    event.preventDefault();
+    setIsDraggedOver(true);
+  };
 
-    const handleDragLeave = (e) => {
-        e.preventDefault();
-        setIsDragOver(false);
-    };
+  /**
+   * Event handler for the 'dragleave' event. Prevents the default behavior and sets isDraggedOver to false.
+   *
+   * @param {Event} event - The dragleave event.
+   */
+  const handleDragLeave = (event) => {
+    event.preventDefault();
+    setIsDraggedOver(false);
+  };
 
-    const handleDrop = (e) => {
-        e.preventDefault();
-        const image = e.dataTransfer.files[0];
-        imageFile(image);
-        handleFileUpload(image);
-    };
+  /**
+   * Event handler for the 'drop' event. Prevents the default behavior, retrieves the dropped image,
+   * calls the imageFile function with the image, sets the imageDataUrl and isImageUploaded state variables,
+   * and sets isDraggedOver to false.
+   *
+   * @param {Event} event - The drop event.
+   */
+  const handleDrop = (event) => {
+    event.preventDefault();
+    const image = event.dataTransfer.files[0];
+    imageFile(image);
+    setImageDataUrl(URL.createObjectURL(image));
+    setIsImageUploaded(true);
+    setIsDraggedOver(false);
+  };
 
-    const handleFileUpload = (file) => {
-        const validExtensions = ["image/jpeg", "image/jpg", "image/png"];
-        if (validExtensions.includes(file.type)) {
-            const reader = new FileReader();
-            reader.onloadend = () => {
-                setUploadedImage(reader.result);
-                setIsImageUploaded(true);
-            };
-            reader.readAsDataURL(file);
-        } else {
-            setIsImageUploaded(false);
-        }
-    };
+  /**
+   * Event handler for the 'click' event of the delete button. Sets the imageDataUrl and isImageUploaded
+   * state variables to their initial values.
+   */
+  const handleDelete = () => {
+    setImageDataUrl('');
+    setIsImageUploaded(false);
+  };
 
-    const handleDelete = () => {
-        setUploadedImage('');
-        setIsImageUploaded(false);
-    };
+  return (
+    <div className="dragger-wrapper">
+      {/* The drag and drop area */}
+      <div
+        className={`dragger ${isImageUploaded ? 'active' : ''}`}
+        id="dragger"
+        onDragOver={handleDragOver}
+        onDragLeave={handleDragLeave}
+        onDrop={handleDrop}
+      >
+        {isImageUploaded ? (
+          // If an image has been uploaded, display the image
+          <img src={imageDataUrl} alt="" />
+        ) : (
+          <>
+            {/* If no image has been uploaded, display a message and a file input field */}
+            <h2>{isDraggedOver ? 'Release to Upload Image' : 'Drag and Drop Files'}</h2>
+            <h3>or</h3>
 
-    return (
-        <div className="dragger-wrapper">
-            <div
-                className={`dragger ${isImageUploaded ? ' active' : ''}`}
-                id="dragger"
-                onDragOver={handleDragOver}
-                onDragLeave={handleDragLeave}
-                onDrop={handleDrop}
-            >
-                {isImageUploaded ? (
-                    <img src={uploadedImage} alt="" />
-                ) : (
-                    <>
-                        <h2>{isDragOver ? 'Release to Upload Image' : 'Drag and Drop Files'}</h2>
-                        <h3>or</h3>
-
-                        <button className="browse-file" id="browse-file">
-                            Browse File
-                        </button>
-                        <input type="file" hidden id="file-input-field" />
-                    </>
-                )}
-            </div>
-            <div className="file-name" id="file-name">
-                {isImageUploaded ? (
-                    <i className="fas fa-trash-can" onClick={handleDelete}>delete</i>
-                ) : (
-                    ''
-                )}
-            </div>
-        </div>
-    );
+            <button className="browse-file" id="browse-file">
+              Browse File
+            </button>
+            <input type="file" hidden id="file-input-field" />
+          </>
+        )}
+      </div>
+      {/* The delete button */}
+      <div className="file-name" id="file-name">
+        {isImageUploaded ? (
+          <i className="fas fa-trash-can" onClick={handleDelete}>
+            delete
+          </i>
+        ) : (
+          ''
+        )}
+      </div>
+    </div>
+  );
 };
 
 DragAndDrop.propTypes = {
