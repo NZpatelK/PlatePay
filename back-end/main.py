b6d40b283b2036e9ab9f39f45cd16896d36ac7ab
NZpatelK <patelk.dev@hotmail.com>
2024-07-18 13:45:59 +1200

    testing data - successful but little lagging

--- back-end/main.py
+++ back-end/main.py
@@ -1,16 +1,22 @@
+import random
+import time
+
 from flask import Flask, jsonify, request
 from flask_socketio import SocketIO, send
 from flask_cors import CORS
 import number_plate_recognition
-
-# app = Flask(__name__)
-# app.config['SECRET_KEY'] = 'mysecret'
-# socketio = SocketIO(app, cors_allowed_origins="http://localhost:5173")
-# CORS(app, resources={r"/*": {"origins": "http://localhost:5173"}})
+from threading import Thread
 
 app = Flask(__name__)
-CORS(app, resources={r"/api/*": {"origins": "http://localhost:5173"}})
+app.config['SECRET_KEY'] = 'mysecret'
 socketio = SocketIO(app, cors_allowed_origins="http://localhost:5173")
+CORS(app, resources={r"/*": {"origins": "http://localhost:5173"}})
+
+# app = Flask(__name__)
+# CORS(app, resources={r"/api/*": {"origins": "http://localhost:5173"}})
+# socketio = SocketIO(app, cors_allowed_origins="http://localhost:5173")
+
+
 
 # @app.route('/api/data', methods=['GET'])
 # def get_data():
@@ -23,20 +29,18 @@ socketio = SocketIO(app, cors_allowed_origins="http://localhost:5173")
 #     print('Message: ' + msg)
 #     send(msg, broadcast=True)
 
+def generate_random_numbers():
+    rounds = 0
+    max_rounds = 10
+    while True:
+        value = input("enter: ")
+        socketio.emit('random_number', {'number': value})
+
 @app.route('/api/upload', methods=['POST'])
 def upload_image():
     print("got images")
     file = request.files['image']
     file.save('temp.jpg')
-
-    # # Read the image
-    # img = cv2.imread('temp.jpg')
-    #
-    # # Use pytesseract to do OCR on the image
-    # text = pytesseract.image_to_string(img)
-    #
-    # # Assuming number plate text is extracted successfully
-    # plate_number = text.strip()  # Clean up the extracted text
 
     plate_number = number_plate_recognition.get_plate_number('temp.jpg')
 
@@ -46,5 +50,9 @@ def upload_image():
 
     return jsonify({'message': 'Processing started'})
 
+
 if __name__ == '__main__':
+    thread = Thread(target=generate_random_numbers)
+    thread.daemon = True
+    thread.start()
     socketio.run(app, debug=True)
